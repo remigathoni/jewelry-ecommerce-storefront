11b7a2a56e0e5a0f5de81052ab116451341ec257
gathoni-k <maryg.kahiga@gmail.com>
2022-10-07 10:30:13 +0300

    Create navigation (#9)
    * Match lint rules
    
    * AH-17 Add tailwindcss
    
    * AH-1 Create main menu 
    
    * Add google font
    
    * Install sass
    
    * AH-1 Add svg icons for main menu
    
    * AH-1 Delete tailwind directives
    
    * AH-1 Create main menu
    
    * Ah 2 create main mobile menu (#7)
    
    * AH-2 Add labels to icon buttons
    
    * AH-2 Create mobile menu

---
+++ components/navigation/MainNav.tsx
@@ -0,0 +1,82 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../logo/Logo";
+import styles from "./mainnav.module.scss"
+export default function MainNav() {
+  return (
+    <>
+      <nav className={styles.container}>
+        <Logo/>
+        <ul className={styles.links}>
+          <li>
+            <Link href="/">
+              <a>NECKLACES</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>EARRINGS</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>JEWELRY CARE</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>ABOUT</a>
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <button aria-label="search">     
+              <Image src="/search.svg" alt="search" width={24} height={24}/>
+            </button>
+          </li>
+          <li>
+            <button aria-label="account">     
+              <Image src="/account.svg" alt="account" width={24} height={24}/>
+            </button>
+          </li>
+          <li>
+            <button aria-label="cart">    
+              <Image src="/cart.svg" alt="cart" width={24} height={24}/>
+            </button>
+          </li>
+          <li className={styles.menu}>
+            <button aria-label="menu">    
+              <Image src="/menu.svg" alt="" width={24} height={24}/>
+            </button>
+          </li>
+        </ul>
+
+      </nav>
+      <nav className={styles.mobile}>
+        <ul >
+          <li>
+            <Link href="/">
+              <a>NECKLACES</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>EARRINGS</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>JEWELRY CARE</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>ABOUT</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+
+import Document, { DocumentContext, DocumentInitialProps ,Html, Head, Main, NextScript } from "next/document"
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
