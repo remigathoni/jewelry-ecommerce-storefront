5474d63a5f5bd3138e9be1ec5381d6a4dc8d2edb
gathoni-k <maryg.kahiga@gmail.com>
2022-10-07 05:17:19 +0300

    AH-1 Create main menu
    * Add google font
    
    * Install sass
    
    * AH-1 Add svg icons for main menu
    
    * AH-1 Delete tailwind directives
    
    * AH-1 Create main menu

---
+++ components/logo/Logo.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <Image src="/logo.svg" alt="" width={100} height={30}/>
+  )
+}
---
+++ components/navigation/MainNav.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../logo/Logo";
+import styles from "./mainnav.module.scss"
+export default function MainNav() {
+  return (
+    <nav className={styles.container}>
+      <Logo/>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>NECKLACES</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>EARRINGS</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>JEWELRY CARE</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>ABOUT</a>
+          </Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <button>     
+            <Image src="/search.svg" alt="search" width={24} height={24}/>
+          </button>
+        </li>
+        <li>
+          <button>     
+            <Image src="/account.svg" alt="account" width={24} height={24}/>
+          </button>
+        </li>
+        <li>
+          <button>    
+            <Image src="/cart.svg" alt="cart" width={24} height={24}/>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  )
+}
