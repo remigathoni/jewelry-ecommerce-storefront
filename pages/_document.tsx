e09bda35adfc54bdeb2e100409b2f9ca9b9f165c
gathoni-k <maryg.kahiga@gmail.com>
2022-10-08 06:22:02 +0300

    AH-4 Add lora font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,9 +13,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
-          <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500&display=swap" rel="stylesheet"></link>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+          <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500&family=Lora:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
