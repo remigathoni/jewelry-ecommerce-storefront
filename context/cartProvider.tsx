fe2a1f5a6b91320fd59b6d0564dca9b67c83ddcc
Mary Gathoni <maryg.kahiga@gmail.com>
2023-07-10 19:07:02 +0300

    Create add to cart flow

---
+++ context/cartProvider.tsx
@@ -0,0 +1,34 @@
+import { ReactNode, useState } from "react";
+import { product } from "../lib/types/product.type";
+import { CartContext, CartContextType } from "./cartContext";
+
+const CartProvider = ({ children }:{children: ReactNode}) => {
+  const [cartItems, setcartItems] = useState<product[]>([])
+
+  const addToCart = (product:product) => {
+    const updatedCart:product[] = [...cartItems, product]
+    setcartItems(updatedCart)  };
+
+  const removeFromCart = (product: product) => {
+    const updatedCart = cartItems.filter(
+      (currentProduct) => currentProduct.id !== product.id
+
+    );
+    setcartItems(updatedCart)
+  }
+  const clearCart = () => {
+    setcartItems([]);
+  };
+
+  const cartContextValue: CartContextType = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+    clearCart,
+  };
+  return (
+    <CartContext.Provider value={cartContextValue}>{children}</CartContext.Provider>
+  )
+};
+
+export default CartProvider
---
+++ pages/cart.tsx
@@ -0,0 +1,11 @@
+import Cart from "../components/Cart/Cart"
+import MainNav from "../components/navigation/MainNav"
+
+export default function cart() {
+  return (
+    <div>
+      <MainNav/>
+      <Cart/>
+    </div>
+  )
+}
