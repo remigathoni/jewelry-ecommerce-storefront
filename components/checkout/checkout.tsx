93987bcafec5d7d3682bd0ba975436d735d89bc6
Mary Gathoni <maryg.kahiga@gmail.com>
2023-07-31 17:34:14 +0300

    Update layout

--- components/checkout/checkout.tsx
+++ components/checkout/checkout.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { useCartContext } from "../../context/cartContext";
-import LinkButton from "../linkButton/LinkButton";
+import supabase from "../../lib/supabase/supabase";
 import styles from "./checkout.module.scss";
 
 function Checkout() {
@@ -14,59 +14,52 @@ function Checkout() {
       console.log("Please select your shipping address");
       return
     }
+    if(!cartItems) {
+      console.log("Add items")
+      return
+    }
     // move to stripe checkoutpage
     try {
+      
+      const {data:{user}, error} = await supabase.auth.getUser() 
       const { data } = await axios.post(
         "http://localhost:3000/api/orders/checkout",
         {
           items: cartItems,
           shippingInfo,
+          user
         }
       );
 
       console.log(data)
-      if(data.url) window.location.href = data.url;
+      // if(data.url) window.location.href = data.url;
     } catch (error:any) {
       console.log(error.response);
     }
   };
+
+  const handleSubmit = async (event:any) => {
+    event.preventDefault()
+    await checkoutHandler()
+  }
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <h1>CHECKOUT</h1>
         <small>Kindly fill in your personal information and complete the payment.</small>
       </div>
-      <section className={styles.shippingsection}>
-        {/* <div className={styles.inputSection}>
-          <div className={styles.inputRow}>
-            <label htmlFor="firstname">First Name</label>
-            <input type="text" name="first name" id="firstname"/>
+      <form onSubmit={handleSubmit}>
+        <section className={styles.shippingsection}>
+          <div className={styles.inputRowFull}>
+            <label htmlFor="deliveryaddress">Delivery address</label>
+            <input required type="text" name="delivery address" id="deliveryaddress" value={shippingInfo} onChange={(e) => setShippingInfo(e.target.value)}/>
           </div>
-          <div className={styles.inputRow}>
-            <label htmlFor="lastname">Last Name</label>
-            <input type="text" name="last name" id="lastname"/>
-          </div>
-        </div>
 
-        <div className={styles.inputSection}>
-          <div className={styles.inputRow}>
-            <label htmlFor="phonenumber">Phone number</label>
-            <input type="tel" name="phone number" id="phone number"/>
-          </div>
-          <div className={styles.inputRow}>
-            <label htmlFor="lastname">Email</label>
-            <input type="text" name="last name" id="lastname"/>
-          </div>
-        </div> */}
+        </section>
+        <button type="submit" className={styles.button}>Proceed to Payment</button>
 
-        <div className={styles.inputRowFull}>
-          <label htmlFor="deliveryaddress">Delivery address</label>
-          <input type="text" name="delivery address" id="deliveryaddress" value={shippingInfo} onChange={(e) => setShippingInfo(e.target.value)}/>
-        </div>
-
-      </section>
-      <button onClick={checkoutHandler}>Go to checkout </button>
-      <LinkButton text={"Proceed to payment"} background={"#eee"} link={"/"} />
+      </form>
+      
     </div>
   )
 }
